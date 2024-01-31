37ddf861987f371d569dea376f75bf6b4db0b42c
Ali <100485408+ixsalimo@users.noreply.github.com>
2024-01-31 19:19:49 +0330

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 const $ = document , _id = id => $.getElementById(id) , _qs = id => $.querySelector(id) , _qsa = id => $.querySelectorAll(id);
 //! Functions
-!function (DATE = new Date()) {
+window.addEventListener("DOMContentLoaded" , function () {
     let bg = _id('particles-js') , icon = _qs('link[rel="icon"]') , tab = _qs('meta[name="theme-color"]');
     switch (DATE.getUTCMonth() + 1) {
         case 12:
@@ -8,7 +8,7 @@ const $ = document , _id = id => $.getElementById(id) , _qs = id => $.querySelec
         case 2:
             tab.content = '#a0c4ff';
             bg.className = 'winter';
-            icon.href = '../Pic/Tab/winter.png';
+            icon.setAttribute("href" , '../Pic/Tab/winter.png' );
             particlesJS.load('particles-js', '../Libraries/JSON/winter-bg.json');
             break;
         case 3:
@@ -16,27 +16,64 @@ const $ = document , _id = id => $.getElementById(id) , _qs = id => $.querySelec
         case 5:
             tab.content = '#a1ff0a';
             bg.className = 'spring';
-            icon.href = '../Pic/Tab/spring.png';
+            icon.setAttribute("href" , '../Pic/Tab/spring.png' );
             break;
         case 6:
         case 7:
         case 8:
             tab.content = '#ef0';
             bg.className = 'summer';
-            icon.href = '../Pic/Tab/summer.png';
+            icon.setAttribute("href" , '../Pic/Tab/summer.png' );
             break;
         case 9:
         case 10:
         case 11:
             tab.content = '#ff8700';
             bg.className = 'autumn';
-            icon.href = '../Pic/Tab/autumn.png';
+            icon.setAttribute("href" , '../Pic/Tab/autumn.png' );
             particlesJS.load('particles-js', '../Libraries/JSON/autumn-bg.json');
             break;
         default:
             bg.className = 'gradient';
     }
-}();
+});
+// !function (DATE = new Date()) {
+//     let bg = _id('particles-js') , icon = _qs('link[rel="icon"]') , tab = _qs('meta[name="theme-color"]');
+//     switch (DATE.getUTCMonth() + 1) {
+//         case 12:
+//         case 1:
+//         case 2:
+//             tab.content = '#a0c4ff';
+//             bg.className = 'winter';
+//             icon.setAttribute("href" , '../Pic/Tab/winter.png' );
+//             particlesJS.load('particles-js', '../Libraries/JSON/winter-bg.json');
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             tab.content = '#a1ff0a';
+//             bg.className = 'spring';
+//             icon.setAttribute("href" , '../Pic/Tab/spring.png' );
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             tab.content = '#ef0';
+//             bg.className = 'summer';
+//             icon.setAttribute("href" , '../Pic/Tab/summer.png' );
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             tab.content = '#ff8700';
+//             bg.className = 'autumn';
+//             icon.setAttribute("href" , '../Pic/Tab/autumn.png' );
+//             particlesJS.load('particles-js', '../Libraries/JSON/autumn-bg.json');
+//             break;
+//         default:
+//             bg.className = 'gradient';
+//     }
+// }();
 //? Events
 window.addEventListener('load' , () => {_id('loader').classList.add('hide');setTimeout(() => _id('loader').remove() , 1000)});
 window.addEventListener('keydown' , event => (event.key == 'F12' ? event.preventDefault() : (event.key == 'ContextMenu' ? event.preventDefault() : null)));
